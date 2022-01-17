4cb39ef0ad2d6927a23e6110373a2e060a54d254
miro-ring <miro.ring@kakaocorp.com>
2022-01-17 19:04:40 +0900

    feat: 기본 세팅

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import MoneyProvider from './context/MoneyProvider';
 
+ReactDOM.render(
+    <React.StrictMode>
         <MoneyProvider>
+            <App />
         </MoneyProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
