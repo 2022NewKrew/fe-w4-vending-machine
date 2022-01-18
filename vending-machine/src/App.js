f807da171d698a4e24a0f13e9559728708362a31
dobby.shin <dobby.shin@kakaocorp.com>
2022-01-18 15:05:40 +0900

    chore: 콘솔로그 및 빈 함수 정리

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -11,13 +11,7 @@ function App() {
   const [moneyInput, setMoneyInput] = useState(0);
   const fetchInitialData = async (setFunc, URL) => {
     const res = await getData(URL);
-<<<<<<< HEAD
-    await setFunc(res);
-=======
-    console.log(res);
-    await setFunc(res);
-    console.log(products, slots);
->>>>>>> 6d1187d (Feat: 자판기 기본 틀과 props 기본 구성)
+    setFunc(res);
   }
 
   useEffect(() => {
--- vending-machine/src/Wallet.js
+++ vending-machine/src/Wallet.js
@@ -1,10 +1,6 @@
 import './Wallet.css';
 function Wallet({ slots }) {
-<<<<<<< HEAD
   const slotsList = slots.map((slot, idx) =>
-=======
-  const slotsList = slots.map(slot =>
->>>>>>> 6d1187d (Feat: 자판기 기본 틀과 props 기본 구성)
     <li className="wallet__slot">{slot}원</li>
   );
 
@@ -15,8 +11,4 @@ function Wallet({ slots }) {
   );
 }
 
-<<<<<<< HEAD
 export default Wallet;
-=======
-export default Wallet;
->>>>>>> 6d1187d (Feat: 자판기 기본 틀과 props 기본 구성)
