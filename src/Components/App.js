4291507bdbbe78aacca9472788bd87b2b42b4006
brad.ley <brad.ley@kakaocorp.com>
2022-01-18 14:48:49 +0900

    remove unused hooks

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ProductSelectionView from './Product/ProductSelectionView'
 import { InvestmentSelectionView } from './Investment/InvestmentSelectionView'
-import styles from './App.css'
+import './App.css'
 import InvestmentContextProvider from '../Context/InvestmentContext'
 import { ProgressView } from './Progress/ProgressView'
 
 const App = () => {
-  
-  const [investment, setInvestment] = useState(0)
-  
   return (
     <>
       <div className="app">
