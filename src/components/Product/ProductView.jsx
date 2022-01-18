9f1982a5649cbf5064dcc065ea386a98c9acbd19
miro-ring <miro.ring@kakaocorp.com>
2022-01-18 10:51:52 +0900

    chore: 데이터 id 추가

--- src/components/Product/ProductView.jsx
+++ src/components/Product/ProductView.jsx
@@ -1,13 +1,13 @@
-import Product from './Product';
 import styled from 'styled-components';
+import Product from './Product';
 import { wrapContainer } from '../../style';
 
 const items = [
-    { name: '사이다', price: 1000 },
-    { name: '파인애플 맛 환타', price: 400 },
-    { name: '포도맛 환타', price: 300 },
-    { name: '레몬에이드', price: 900 },
-    { name: '봉봉', price: 1200 },
+    { id: 0, name: '사이다', price: 1000 },
+    { id: 1, name: '파인애플 맛 환타', price: 400 },
+    { id: 2, name: '포도맛 환타', price: 300 },
+    { id: 3, name: '레몬에이드', price: 900 },
+    { id: 4, name: '봉봉', price: 1200 },
 ];
 
 const Wrapper = styled.ul`
@@ -21,8 +21,8 @@ const Wrapper = styled.ul`
 `;
 
 export default function ProductView() {
-    const productList = items.map(({ name, price }) => (
-        <Product name={name} price={price} />
+    const productList = items.map(({ id, name, price }) => (
+        <Product key={id} name={name} price={price} />
     ));
 
     return <Wrapper>{productList}</Wrapper>;
--- src/components/Wallet/WalletView.jsx
+++ src/components/Wallet/WalletView.jsx
@@ -5,13 +5,13 @@ import TotalMoney from './TotalMoney';
 import { wrapContainer } from '../../style';
 
 const items = [
-    { num: 0, value: 10 },
-    { num: 1, value: 50 },
-    { num: 5, value: 100 },
-    { num: 5, value: 500 },
-    { num: 0, value: 1000 },
-    { num: 0, value: 5000 },
-    { num: 0, value: 10000 },
+    { id: 0, num: 0, value: 10 },
+    { id: 1, num: 1, value: 50 },
+    { id: 2, num: 5, value: 100 },
+    { id: 3, num: 5, value: 500 },
+    { id: 4, num: 0, value: 1000 },
+    { id: 5, num: 0, value: 5000 },
+    { id: 6, num: 0, value: 10000 },
 ];
 
 const Wrapper = styled.ul`
@@ -22,11 +22,12 @@ const Wrapper = styled.ul`
 export default function WalletView() {
     const [moneyHave, setMoneyHave] = useState(items);
 
-    const unitList = moneyHave.map(({ num, value }, idx) => (
+    const unitList = moneyHave.map(({ id, num, value }) => (
         <MoneyUnit
+            key={id}
+            id={id}
             num={num}
             value={value}
-            idx={idx}
             setMoneyHave={setMoneyHave}
         />
     ));
