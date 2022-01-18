45fafb1b10df812b0faea17b086aa4aba7a37eb4
Remian103 <alsdnska@naver.com>
2022-01-18 14:03:06 +0900

    feat: 로그 추가 시 로그 리스트 최하단이 노출될 수 있도록 useEffect 추가

--- src/components/Progress/ProgressLog.jsx
+++ src/components/Progress/ProgressLog.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useStore } from '../../context/Store';
 
 const Wrapper = styled.div`
     overflow: auto;
-    height: 100px;
+    height: 300px;
     border: 1px solid #000;
+`;
+
+const Log = styled.li`
+    list-style: none;
+    font-size: 20px;
+    line-height: 30px;
 `;
 
 export default function ProgressLog() {
     const { logList } = useStore();
+    const logBoxRef = useRef();
 
-    const logs = logList.map((log) => <li>{log}</li>);
+    const logs = logList.map((log) => <Log>{log}</Log>);
 
-    return <Wrapper>{logs}</Wrapper>;
+    useEffect(() => {
+        if (logBoxRef.current) {
+            logBoxRef.current.scrollTop = logBoxRef.current.scrollHeight;
+        }
+    }, [logList]);
+
+    return <Wrapper ref={logBoxRef}>{logs}</Wrapper>;
 }
